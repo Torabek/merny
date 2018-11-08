171b955db3b6c477651536a7e48e02093c66b1e6
BekhruzMM <b.mirtursinov@student.inha.uz>
2018-11-08 19:49:15 +0500

    usefull links

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ app.get('/', (req, res) => {
   res.send("Bekhruz");
 });
 
+app.post('/', (req, res) => {
+  res.send("Bekrhus post")
+})
+
 app.listen(5000, () => {
   console.log("server is running on port 5000");
 });
