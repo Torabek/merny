f1959d6cc85911522af116b4c48a734ea28be62f
BekhruzMM <b.mirtursinov@student.inha.uz>
2018-11-09 17:04:05 +0500

    some

---
+++ routers/api/users.js
@@ -0,0 +1,121 @@
+/// handle authentication only
+const express = require("express");
+const mongoose = require("mongoose");
+const router = express.Router();
+const User = require('../../models/User');
+const gravatar = require('gravatar');
+const bcryptjs = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const keys = require('../../config/key');
+// @routes GET api/users/test
+// @desc Test users route
+// @access PUBLIC
+
+router.get("/test", (req, res) => {
+  res.json({
+    msg: "users work"
+  });
+});
+
+
+// @routes POST api/users/login
+// @desc Login User
+// @access PUBLIC
+
+router.post('/login', (req, res) => {
+
+  //find user by email
+  const condidate = User.findOne({
+      email: req.body.email
+    })
+    .then(user => {
+      // check for user
+      if (!user) {
+        return res.status(404).json({
+          email: "user not found"
+        });
+      }
+      // password checking
+      bcryptjs.compare(req.body.password, user.password)
+        .then(isMatch => {
+          if (isMatch) {
+
+            // create jwt payload
+            const payload = {
+              id: user.id,
+              name: user.name,
+              avatar: user.avatar
+            }
+
+            jwt.sign(payload, keys.secretorKey, {
+              expiresIn: 3600,
+            }, (err, token) => {
+              res.json({
+                success: true,
+                token: 'Baerer ' + token
+              })
+            });
+
+            res.json({
+              msg: "Success"
+            });
+          } else {
+            res.status(400).json({
+              password: "password didnot match"
+            })
+          }
+        });
+    })
+
+
+});
+
+
+
+
+// @routes POST api/users/register
+// @desc register users
+// @access PUBLIC
+
+router.post('/register', (req, res) => {
+  User.findOne({
+    email: req.body.email
+  }).then(user => {
+    if (user) {
+      return res.status(400).json({
+        email: "email already exist"
+      });
+    } else {
+
+      const avatar = gravatar.url(req.body.email, {
+        s: '200', //kattaligi size
+        r: 'pg', // rating
+        default: "mm"
+      });
+
+      const newuser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        avatar
+      });
+
+      bcryptjs.genSalt(10, (err, salt) => {
+        bcryptjs.hash(newuser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newuser.password = hash;
+          newuser.save()
+            .then(user => {
+              res.json(user)
+            })
+            .catch(err => console.log(err));
+        })
+      });
+
+    }
+  })
+
+});
+
+
+module.exports = router;
