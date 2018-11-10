f5bd618f0282b9abd64225d9eeae5e7441ce7c62
Turabek Gaybullaev <turabek.gaybullaev@gmail.com>
2018-11-10 18:18:49 +0500

    Profile Schema

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
-  mongoURI: "mongodb://turabek:t6rabek@ds163162.mlab.com:63162/bitstreemdb",
+  //mongoURI: "mongodb://turabek:t6rabek@ds163162.mlab.com:63162/bitstreemdb",
+  mongoURI: "mongodb://localhost:27017/bitstreemdb",
   secretOrKey: "BitStreemSecretK"
 };
